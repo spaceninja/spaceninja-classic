e8e091f27b689f33aad1c110cf6b8a9fcfe053f7
Scott Vandehey <fake-scott@spaceninja.com>
2023-08-23 09:18:07 -0700

    remove old PHP junk from design v2

--- public/site-archives/spaceninja/v2/design/cnslct/cnsops.php
+++
@@ -1,9 +0,0 @@
-<? include "head.html"; ?>
-
-<P><FONT SIZE=4><B>The Data Center Operations Team</B></FONT>
-<BR>This team monitors equipment, handles production jobs and does data backup for PSU's central computer area. An operator is on-site 24 hours per day/seven days per week to monitor the 70+ servers and large systems in the PSU Data Center. The team also runs select bulk printing jobs and scantron scanning services for campus departments.
-
-<P>725-3280
-<BR>cns-ops@lists.pdx.edu
-
-<? include "foot.html"; ?>
--- public/site-archives/spaceninja/v2/design/cnslct/index.php
+++
@@ -1,5 +0,0 @@
-<? include "head.html"; ?>
-
-<a href="hire.php"><img src="images/splash.gif" border=0></a>
-
-<? include "foot.html"; ?>
